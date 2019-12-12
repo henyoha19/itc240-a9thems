c57f4dd5c2723f441bcfe92a3f65edf01cd9f117
Henock W <46727864+henyoha19@users.noreply.github.com>
2019-12-12 14:04:53 -0800

    Add files via upload

---
+++ bc/index.php
@@ -0,0 +1,15 @@
+<?php include 'includes/config.php'?> 
+<?php include 'includes/header.php';?>
+<h2> Home Page Content will be here </h2>
+<p> Home Page Content</p>          
+<p> Home Page Content</p>          
+<p> Home Page Content</p>          
+<p> Home Page Content</p>          
+<p> Home Page Content</p>          
+<p> Home Page Content</p>          
+<p> Home Page Content</p>          
+<p> Home Page Content</p>          
+<p> Home Page Content</p>          
+<p> Home Page Content</p>          
+<p> Home Page Content</p>            
+ <?php include 'includes/footer.php';?>
---
+++ bc/recipes_list.php
@@ -0,0 +1,49 @@
+<?php
+//customer_list.php - shows a list of customer data
+/*
+create table Recipes
+( ReceipeID int unsigned not null auto_increment primary key,
+Title varchar(50),
+CookingTime varchar(50),
+Ingredients text, 
+Directions text
+);
+
+insert into Recipes values (NULL,"Perfect Turkey","4 hours","Turkey", "bake at 350");
+*/
+?>
+<?php include 'includes/config.php';?>
+<?php include 'includes/header.php';?>
+
+<?php
+$sql = "select * from Recipes";
+//we connect to the db here
+$iConn = mysqli_connect(DB_HOST,DB_USER,DB_PASSWORD,DB_NAME);
+
+//we extract the data here
+$result = mysqli_query($iConn,$sql);
+
+if(mysqli_num_rows($result) > 0)
+{//show records
+
+    while($row = mysqli_fetch_assoc($result))
+    {
+       
+         echo '<p><a href="receipe_view.php?id=' . $row['ReceipeID'] . '">' . $row['Title'] . '</a></p>';
+        echo '<p><b>Ingredients: </b>' . $row['Ingredients'] . '</p> ';
+        echo '<p><b>Directions: </b>' . $row['Directions'] . '</p> ';
+         echo '<p><img src="uploads/recipes' . $row['ReceipeID'] . '.jpg" /></p>';
+    }    
+
+}else{//inform there are no records
+    echo '<p>There are currently no customers</p>';
+}
+
+//release web server resources
+@mysqli_free_result($result);
+
+//close connection to mysql
+@mysqli_close($iConn);
+
+?>
+<?php include 'includes/footer.php';?>
